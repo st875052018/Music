85245e9896796dbc16566dbd719c915bafcd5b81
GoodyKeng <keng841212@gmail.com>
2020-09-14 21:47:14 +0800

    add newsong

---
+++ scripts/playlisteng.js
@@ -0,0 +1,77 @@
+trackList = [
+	{
+		name:"Ed Sheeran - Castle On The Hill [Official Lyric Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Castle On The Hill [Official Lyric Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Galway Girl [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Galway Girl [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Happier (Official Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Happier (Official Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - I See Fire (Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - I See Fire (Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Perfect (Official Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Perfect (Official Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Photograph (Official Music Video)",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Photograph (Official Music Video).mp3"
+	},
+	{
+		name:"Ed Sheeran - Shape of You [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Shape of You [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - The A Team [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - The A Team [Official Video].mp3"
+	},
+	{
+		name:"Ed Sheeran - Thinking Out Loud [Official Video]",
+		artist:"Ed Sheeran",
+		src:"eng_music/Ed Sheeran - Thinking Out Loud [Official Video].mp3"
+	},
+	{
+		name:"Lukas Graham - 7 Years [OFFICIAL MUSIC VIDEO]",
+		artist:"Lukas Graham",
+		src:"eng_music/Lukas Graham - 7 Years [OFFICIAL MUSIC VIDEO].mp3"
+	},
+	{
+		name:"Lukas Graham - Love Someone [OFFICIAL MUSIC VIDEO]",
+		artist:"Lukas Graham",
+		src:"eng_music/Lukas Graham - Love Someone [OFFICIAL MUSIC VIDEO].mp3"
+	},
+	{
+		name:"What Do I Know - Ed Sheeran [Lyric]",
+		artist:"Ed Sheeran",
+		src:"eng_music/What Do I Know - Ed Sheeran [Lyric].mp3"
+	},
+	{
+		name:"Tyla Yaweh Tommy Lee - Tommy Lee",
+		artist:"Tommy Lee",
+		src:"eng_music/Tyla Yaweh Tommy Lee - Tommy Lee.mp3"
+	},
+	{
+		name:"Multiple Artists - songs to self-isolate to",
+		artist:"TMultiple Artists",
+		src:"eng_music/Multiple Artists - songs to self-isolate to.mp3"
+	},
+	{
+		name:"XXXTENTACION Juice WRLD - where did u go (Ocean Mix)",
+		artist:"XXXTENTACION Juice WRLD",
+		src:"eng_music/XXXTENTACION Juice WRLD - where did u go (Ocean Mix).mp3"
+	}
+];
